6ab1706a3f14e18d51818253fab748cb97b9e09d
xelAhcratiPsavilO <alejandropitarcholivas@gmail.com>
2020-06-11 22:31:38 +0100

    add attack feature

---
+++ spec/features/attack_spec.rb
@@ -0,0 +1,8 @@
+
+feature 'Attack' do
+  scenario 'attack Player 2' do
+    sign_in_and_play
+    click_link 'Attack'
+    expect(page).to have_content 'Alex attacked Player2'
+  end
+end
--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 feature 'Enter names' do
-  scenario 'submitting names' do
+  scenario 'submit names' do
     sign_in_and_play
     expect(page).to have_content 'Alex vs Player2'
   end
